28f5f2361ee1783523a1b7b74650417421d93d9b
KonradWu <seankonri@wp.pl>
2019-02-19 16:45:46 +0100

    Removing hardcoded url, Namespace and Http404 Shortcut

--- music/urls.py
+++ music/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import include, url
 from . import views
 #to jest z tego katalogu
 
+app_name = 'music'
 
 urlpatterns = [
     # /music/
     url('^$', views.index, name='index'),
 
-    # /music/album_id
+    # /music/721/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'), #album_id złożone z cyfr
 ]
