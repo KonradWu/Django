2c6edb196ebebc20ff31a161d84b0a0b96a54733
KonradWu <seankonri@wp.pl>
2019-02-20 23:58:11 +0100

    Generic views

--- music/urls.py
+++ music/urls.py
@@ -1,4 +1,3 @@
-from django.urls import path
 from django.conf.urls import include, url
 from . import views
 #to jest z tego katalogu
@@ -7,11 +6,8 @@ app_name = 'music'
 
 urlpatterns = [
     # /music/
-    url('^$', views.index, name='index'),
+    url('^$', views.IndexView.as_view(), name='index'),
 
     # /music/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
-
-    # /music/<album_id>/favorite
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,32 +1,15 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.shortcuts import render, get_object_or_404
-from .models import Album, Song
+from django.views import generic
+from .models import  Album
 
 
-def index(request):
-    all_albums = Album.objects.all()
-    context = {'all_albums': all_albums}
-    return render(request, 'music/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'object_list' #ta nazwa jest z automatu i w index.html ale możemy zmienić na inną jeśli chcemy to właśnie w tym miejscu
+
+    def get_queryset(self):
+        return Album.objects.all()
 
 
-def detail(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album': album})
-
-
-def favorite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except (KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html', {
-            'album': album,
-            'error_message': "You did not select a valid song",
-        })
-    else:
-        selected_song.is_favorite = True
-        selected_song.save()
-        return render(request, 'music/detail.html', {'album': album})
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
